094385db79aa866983d93477beddb875fe5bb1cb
truco <22969604+truc0@users.noreply.github.com>
2021-04-25 20:26:15 +0800

    feat(users): init users app

--- purchaser/settings.py
+++ purchaser/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'classrooms.apps.ClassroomsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
